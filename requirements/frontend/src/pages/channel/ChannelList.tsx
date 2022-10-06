a12f6a15682c5ab91b72a7ae6fd4640ee516018c
Arat5724 <arat5724@gmail.com>
2022-10-06 11:05:11 +0900

    feat: :art: 채팅방 리스트 컴포넌트 구현, 하단 구역 사이즈 수정
    채팅방 컴포넌트 구현하고, 하단 구역 사이즈 180px에서 120px로 수정했습니다.

--- requirements/frontend/src/pages/channel/ChannelList.tsx
+++ requirements/frontend/src/pages/channel/ChannelList.tsx
@@ -20,24 +20,24 @@ const array0: Array<ChannelInterface> = [
     lock: true,
   },
   {
-    roomId: 1,
-    title: '모든 채팅',
-    owner: 'jeongble',
-    userCount: 10,
-    lock: true,
+    roomId: 201,
+    title: '자함을 국회로!!!!!!!',
+    owner: 'jaham',
+    userCount: 99,
+    lock: false,
   },
   {
-    roomId: 1,
-    title: '모든 채팅',
-    owner: 'jeongble',
+    roomId: 200,
+    title: 'cjeon의 Haskell 강의',
+    owner: 'cjeon',
     userCount: 10,
-    lock: true,
+    lock: false,
   },
   {
-    roomId: 1,
-    title: '모든 채팅',
-    owner: 'jeongble',
-    userCount: 10,
+    roomId: 404,
+    title: 'ljeongin의 minishell은 언제 끝날 것인가',
+    owner: 'rjeongin',
+    userCount: 44,
     lock: true,
   },
 ];
@@ -114,7 +114,22 @@ export default function ChannelList() {
           ))}
         </ol>
       </div>
-      <div className={styles.channel__footer}></div>
+      <div className={styles.channel__footer}>
+        <button
+          className={`${styles.channel__new} ${
+            filter == 0 || filter == 1 ? '' : styles.channel__inactive
+          }`}
+        >
+          새 채팅방!
+        </button>
+        <button
+          className={`${styles.channel__new} ${
+            filter == 2 ? '' : styles.channel__inactive
+          }`}
+        >
+          새 DM!
+        </button>
+      </div>
     </div>
   );
 }
