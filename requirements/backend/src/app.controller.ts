7aa287e52012c8d93c70163c7066df0e06f676ca
jpham005 <jpham005@naver.com>
2022-10-09 02:13:07 +0900

    style: 백엔드 코드 복구

--- requirements/backend/src/app.controller.ts
+++ requirements/backend/src/app.controller.ts
@@ -1,19 +1,5 @@
-import {
-  Controller,
-  Get,
-  Redirect,
-  Response,
-  Next,
-  Header,
-  Post,
-  Req,
-} from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { DbFriendListService } from './database/db.friend.list/db.friend.list.service';
-import { FriendListEntity } from './database/entity/entity.friend.list';
-import { DbUserService } from './database/db.user/db.user.service';
-import { UserEntity } from './database/entity/entity.user';
 
 function hello(): string {
   return 'hello';
@@ -22,74 +8,8 @@ function hello(): string {
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('users/me')
-  getMyProfile() {
-    return {
-      id: 99945,
-      displayName: 'jaham',
-      imgUri:
-        'https://ca.slack-edge.com/T039P7U66-U01GAGE28SE-4b0009a95b5a-512',
-      rating: 2048,
-    };
-  }
-
-  @Post('users/me')
-  updateMyProfile(@Req() req: Request) {
-    const body = req.body;
-    console.log(body);
-  }
-
-  @Get('users/friend')
-  getFriendList() {
-    return [
-      {
-        id: 1,
-        displayName: 'jaham',
-        imgUri:
-          'https://ca.slack-edge.com/T039P7U66-U01GAGE28SE-4b0009a95b5a-512',
-        rating: 2048,
-      },
-      {
-        id: 2,
-        displayName: 'jeongble',
-        imgUri:
-          'https://ca.slack-edge.com/T039P7U66-U01GAGE28SE-4b0009a95b5a-512',
-        rating: 1024,
-      },
-      {
-        id: 3,
-        displayName: 'yeju',
-        imgUri:
-          'https://ca.slack-edge.com/T039P7U66-U01GAGE28SE-4b0009a95b5a-512',
-        rating: 4096,
-      },
-    ];
-  }
-
-  @Get('users/rank')
-  getRankList() {
-    return [
-      {
-        id: 2,
-        displayName: 'jeongble',
-        imgUri:
-          'https://ca.slack-edge.com/T039P7U66-U01GAGE28SE-4b0009a95b5a-512',
-        rating: 4096,
-      },
-      {
-        id: 3,
-        displayName: 'yeju',
-        imgUri:
-          'https://ca.slack-edge.com/T039P7U66-U01GAGE28SE-4b0009a95b5a-512',
-        rating: 2048,
-      },
-      {
-        id: 1,
-        displayName: 'jaham',
-        imgUri:
-          'https://ca.slack-edge.com/T039P7U66-U01GAGE28SE-4b0009a95b5a-512',
-        rating: 1024,
-      },
-    ];
+  @Get()
+  getHello() {
+    return this.appService.getHello();
   }
 }
