4934b2792b19bcc5baf8caa468251196ec596095
Arat5724 <arat5724@gmail.com>
2022-10-10 12:34:00 +0900

    style: CSS 위치 수정

--- requirements/frontend/src/pages/room/chat/ChatUser.tsx
+++ requirements/frontend/src/pages/room/chat/ChatUser.tsx
@@ -2,24 +2,18 @@ import * as React from 'react';
 import { UserDataType } from '../../../atoms/userDataType';
 import styles from '../../../components/SideBar/Li/UserLi.module.css';
 
-type roleType = 'owner' | 'op' | 'admin'; // todo
+type roleType = 'owner' | 'op' | 'user'; // todo
 
 export interface ChatUserType extends UserDataType {
   role: roleType;
 }
 
 export function ChatUser({ user }: { user: ChatUserType }) {
-  const [isOpen, setIsOpen] = React.useState(false);
-  const onClick = () => {
-    setIsOpen((prev) => !prev);
-  };
+  const onClick = () => {};
 
   return (
     <>
-      <li
-        className={`${styles.user} ${isOpen ? styles.user__active : ''}`}
-        onClick={onClick}
-      >
+      <li className={`${styles.user}`} onClick={onClick}>
         <div className={styles.user_profile__div}>
           <img src={user.imgUri} className={styles.user_profile__img} />
           <div className={styles.user_profile__username}>
