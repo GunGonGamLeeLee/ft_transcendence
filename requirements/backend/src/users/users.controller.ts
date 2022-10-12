8eab8e1b41967ff8482fb1953148f0fc74336b4d
niamu01 <yeju@student.42seoul.kr>
2022-10-12 20:04:44 +0900

    refactor?: method별로 모으는 todo 했습니당
    console.log도 지움

--- requirements/backend/src/users/users.controller.ts
+++ requirements/backend/src/users/users.controller.ts
@@ -30,7 +30,6 @@ import { ProfileUpdateDto } from './dto/profile.update.dto';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  //TODO - method 순서대로 정렬 필요!
   //NOTE - GET
   @ApiOperation({ summary: '내 프로필 가져오기' })
   @ApiResponse({ status: 200, description: '본인 정보 조회 성공' })
@@ -41,6 +40,40 @@ export class UsersController {
     return await this.usersService.me(uid);
   }
 
+  @ApiOperation({ summary: '내 친구 목록 가져오기' })
+  @Get('friend')
+  async friend(@MyUid() uid: number) {
+    return await this.usersService.friend(uid);
+  }
+
+  @ApiOperation({ summary: '내 차단 목록 가져오기' })
+  @Get('blocklist')
+  async blocklist(@MyUid() uid: number) {
+    return await this.usersService.blocklist(uid);
+  }
+
+  @ApiOperation({ summary: '랭킹 목록 가져오기' })
+  @Get('rank')
+  async rank() {
+    return await this.usersService.rank();
+  }
+  
+  @ApiOperation({ summary: '전적 보기 (최신 5게임)' })
+  @ApiQuery({ name: 'uid' })
+  @Get('match')
+  async match(@Query('uid') uid) {
+    return await this.usersService.match(uid, 5, 1);
+  }
+
+  @ApiOperation({ summary: '이미지 uri' })
+  @Get('img/:id')
+  async getImage(@Param('id') param: number) {
+    const imgData = await this.usersService.getImage(+param);
+
+    return imgData;
+  }
+
+  //NOTE - POST
   @ApiOperation({ summary: '내 프로필 업데이트' })
   @ApiResponse({ status: 201, description: '유저 정보 수정' })
   @ApiResponse({ status: 400, description: '지원하지 않는 이미지 형식' })
@@ -61,34 +94,10 @@ export class UsersController {
     if (result == false) throw new HttpException('Bad Request', 400);
   }
 
-  @ApiOperation({ summary: '내 친구 목록 가져오기' })
-  @Get('friend')
-  async friend(@MyUid() uid: number) {
-    return await this.usersService.friend(uid);
-  }
-
-  @ApiOperation({ summary: '내 차단 목록 가져오기' })
-  @Get('blocklist')
-  async blocklist(@MyUid() uid: number) {
-    return await this.usersService.blocklist(uid);
-  }
-
-  @ApiOperation({ summary: '랭킹 목록 가져오기' })
-  @Get('rank')
-  async rank() {
-    return await this.usersService.rank();
-  }
-
   @ApiOperation({ summary: '친구 추가(팔로우)' })
   @Post('follow')
   async follow(@MyUid() uid: number, @Body() body: UidDto) {
     return await this.usersService.follow(uid, body.uid);
-  }
-
-  @ApiOperation({ summary: '친구 삭제(언팔로우)' })
-  @Delete('follow')
-  async unfollow(@MyUid() uid: number, @Body() body: UidDto) {
-    return await this.usersService.unfollow(uid, body.uid);
   }
 
   @ApiOperation({ summary: '차단 하기' })
@@ -96,25 +105,17 @@ export class UsersController {
   async block(@MyUid() uid: number, @Body() body: UidDto) {
     return await this.usersService.block(uid, body.uid);
   }
+  
+  //NOTE - DELETE
+  @ApiOperation({ summary: '친구 삭제(언팔로우)' })
+  @Delete('follow')
+  async unfollow(@MyUid() uid: number, @Body() body: UidDto) {
+    return await this.usersService.unfollow(uid, body.uid);
+  }
 
   @ApiOperation({ summary: '차단 해제' })
   @Delete('block')
   async unblock(@MyUid() uid: number, @Body() body: UidDto) {
     return await this.usersService.unblock(uid, body.uid);
-  }
-
-  @ApiOperation({ summary: '전적 보기 (최신 5게임)' })
-  @ApiQuery({ name: 'uid' })
-  @Get('match')
-  async match(@Query('uid') uid) {
-    return await this.usersService.match(uid, 5, 1);
-  }
-
-  @ApiOperation({ summary: '이미지 uri' })
-  @Get('img/:id')
-  async getImage(@Param('id') param: number) {
-    const imgData = await this.usersService.getImage(+param);
-
-    return imgData;
   }
 }
