070156a3cf7cd212bddec344e696957f52cdac79
Arat5724 <arat5724@gmail.com>
2022-10-12 22:16:47 +0900

    feat: :sparkles: 색깔 전역으로 선언
    ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ

--- requirements/frontend/src/modal/ChatProfileModal/ChatProfileModal.tsx
+++ requirements/frontend/src/modal/ChatProfileModal/ChatProfileModal.tsx
@@ -54,6 +54,7 @@ function ChatProfile({ user }: { user: ChatUserType }) {
         <div className={modalstyles.modal__blank} onClick={onClick}></div>
         <div className={styles.profile}>
           <div className={styles.profile__header}>
+            <span className={styles.profile__headertitle}>PROFILE</span>
             <RedCross onClick={onClick} />
           </div>
           <div className={styles.profile__display}>
