356c1875bbddd4e21fe346e5b7e35cc469c26bcb
jpham005 <jpham005@naver.com>
2022-10-12 19:50:25 +0900

    fix: status 수정, displayname 8글자로 제한, 폰트 미변동
    MMMMMMMM은 삐져나오긴 합니다
    
    99

--- requirements/frontend/src/components/SideBar/FriendList.tsx
+++ requirements/frontend/src/components/SideBar/FriendList.tsx
@@ -1,9 +1,10 @@
 import { useRecoilValue } from 'recoil';
 import { friendListState } from '../../atoms/friendListState';
 import { UserDataType } from '../../atoms/userDataType';
-import styles from './UserList.module.css';
 import { useSetRecoilState } from 'recoil';
 import { userProfileModalState } from '../../atoms/modals/userProfileModalState';
+import { Status } from '../../atoms/userDataType';
+import styles from './UserList.module.css';
 
 export function FriendList() {
   const friendList = useRecoilValue(friendListState);
@@ -30,7 +31,13 @@ function Friend({ user }: { user: UserDataType }) {
         <img src={user.imgUri} className={styles.user_profile__img} />
         <div className={styles.user_profile__username}>{user.displayName}</div>
       </div>
-      <div className={styles.user_state__div}>{user.status}</div>
+      <div className={styles.user_state__div}>
+        {user.status === Status.OFFLINE
+          ? 'offline'
+          : user.status === Status.ONLINE
+          ? 'online'
+          : 'gaming'}
+      </div>
     </li>
   );
 }
