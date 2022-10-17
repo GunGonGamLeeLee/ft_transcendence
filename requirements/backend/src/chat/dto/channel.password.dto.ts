6c0d8cfd453090ed4f4c78d1352d7d8d8e882117
niamu01 <yeju@student.42seoul.kr>
2022-10-17 12:20:22 +0900

    refactor: ApiProperty 데코레이터 제거
    - #149

--- requirements/backend/src/chat/dto/channel.password.dto.ts
+++ requirements/backend/src/chat/dto/channel.password.dto.ts
@@ -1,11 +1,8 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString, Length } from 'class-validator';
 
 export class ChannelPasswordDto {
-  @ApiProperty()
   @IsNumber()
   chid: number;
-  @ApiProperty()
   @Length(0, 4)
   @IsString()
   password?: string;
