fdded75c2052e53cb7b9b9d4486cc194502b1804
hyojekim <42.4.hyojekim@gmail.com>
2022-10-18 14:30:57 +0900

    feat: 채팅방 초대 api 구현
    - #172

--- requirements/backend/src/chat/chat.controller.ts
+++ requirements/backend/src/chat/chat.controller.ts
@@ -16,10 +16,13 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { UserInChannelDto } from 'src/database/dto/user.in.channel.dto';
 import { ChannelMode } from 'src/database/entity/entity.channel';
 import { MyUid } from 'src/users/decorator/uid.decorator';
+import { ChatGateway } from './chat.gateway';
 import { ChatRoomListService } from './chat.room.list.service';
 import { ChatRoomUsersService } from './chat.room.users.service';
+import { ChatService } from './chat.service';
 import { ChannelCreateDto } from './dto/channel.create.dto';
 import { ChannelPasswordDto } from './dto/channel.password.dto';
 
@@ -31,12 +34,14 @@ export class ChatController {
   constructor(
     private readonly chatRoomListService: ChatRoomListService,
     private readonly chatRoomUsersService: ChatRoomUsersService,
+    private readonly chatGateway: ChatGateway,
+    private readonly chatService: ChatService,
   ) {}
 
   //NOTE - GET
   @ApiOperation({ summary: '방 정보 가져오기' })
   @Get('channel')
-  async roomlist(@MyUid() uid) {
+  async roomList(@MyUid() uid) {
     const ret = await this.chatRoomListService.getRoomList(uid);
     return ret;
   }
@@ -44,7 +49,7 @@ export class ChatController {
   @ApiOperation({ summary: '방의 유저 정보 가져오기' })
   @ApiQuery({ name: 'chid' })
   @Get('channelusers')
-  async roomusers(@Query('chid', ParseIntPipe) chid) {
+  async roomUsers(@Query('chid', ParseIntPipe) chid) {
     const ret = await this.chatRoomUsersService.getRoomUsers(chid);
     return ret;
   }
@@ -63,7 +68,7 @@ export class ChatController {
 
   @ApiOperation({ summary: '방 생성하기' })
   @Post('channel')
-  async newroom(@MyUid() uid, @Body() body: ChannelCreateDto) {
+  async newRoom(@MyUid() uid, @Body() body: ChannelCreateDto) {
     if (body.mode === ChannelMode.protected && body.password.length !== 4) {
       throw new HttpException(
         '비밀번호는 4글자이어야 합니다.',
@@ -83,5 +88,14 @@ export class ChatController {
       body.password,
     );
     return this.chatRoomListService.getCreatedRoom(ret);
+  }
+
+  @ApiOperation({ summary: '유저 초대하기' })
+  @Post('invite')
+  async inviteUser(@MyUid() uid, @Body() body: UserInChannelDto) {
+    if (await this.chatService.isInBlockList(body.uid, uid))
+      throw new HttpException('초대에 실패했습니다.', HttpStatus.FORBIDDEN);
+
+    await this.chatGateway.handleInvitation(body);
   }
 }
