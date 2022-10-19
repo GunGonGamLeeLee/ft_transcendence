d8d7431b8e07341a48f9705c5f471a6095c54e68
42mypark <skyttl34@gmail.com>
2022-10-19 19:10:10 +0900

    feat: prod 환경 구축 2
    -  #24
    
     빌드, 운영 환경 분리.
    
    wip env
    
    fix: 필요없는 것들 삭제

--- requirements/.prod/builder.backend.sh
+++ requirements/.prod/builder.backend.sh
@@ -1,22 +1,8 @@
 #!/bin/sh
-
-COUNT=0;
-$( nc -zv database $DBPORT );
-until [ $? -eq 0 ]
-do
-	sleep 10;
-	if [ $COUNT -eq 6 ]
-	then
-		exit 1;
-	fi
-	COUNT=$((COUNT+1))
-	$( nc -zv database $DBPORT );
-done;
-
 if [[ ! -z "$HOST_IP" ]]
 then
 	sed -i "s/localhost/$HOST_IP/g" /prod.backend.env
 fi
 sed -i "s/plz-input-api-uid/$API_UID/g" /prod.backend.env
 sed -i "s/plz-input-api-secret/$API_SECRET/g" /prod.backend.env
-exec node /app/dist/main.js
+/app/node_modules/.bin/nest build
---
+++ requirements/.prod/builder.frontend.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [[ ! -z "$HOST_IP" ]]
+then
+	sed -i "s/localhost/$HOST_IP/g" /app/.env
+fi
+
+npm run build
